3e1715d2c7518cf84ade9cbd2364792735b2093b
Alma <a.q.frankenstein@gmail.com>
2020-09-28 13:30:32 -0700

    use album examples as template
    Co-authored-by: Vanessa Stewart <vstewart1213@gmail.com>

---
+++ app.rb
@@ -0,0 +1,56 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/album')
+require('pry')
+also_reload('lib/**/*.rb')
+
+get('/test') do
+  @something = "this is a variable"
+  erb(:whatever)
+end
+
+get('/') do
+  @albums = Album.all
+  erb(:albums)
+end
+
+get('/albums') do
+  @albums = Album.all
+  erb(:albums)
+end
+
+get('/albums/new') do
+  erb(:new_album)
+end
+
+post('/albums') do
+  name = params[:album_name]
+  album = Album.new(name, nil)
+  album.save()
+  @albums = Album.all() # Adding this line will fix the error
+  erb(:albums)
+end
+
+get('/albums/:id') do
+  @album = Album.find(params[:id].to_i())
+  erb(:album)
+end
+
+get('/albums/:id/edit') do
+  @album = Album.find(params[:id].to_i())
+  erb(:edit_album)
+end
+
+patch('/albums/:id') do
+  @album = Album.find(params[:id].to_i())
+  @album.update(params[:name])
+  @albums = Album.all
+  erb(:albums)
+end
+
+delete('/albums/:id') do
+  @album = Album.find(params[:id].to_i())
+  @album.delete()
+  @albums = Album.all
+  erb(:albums)
+end
