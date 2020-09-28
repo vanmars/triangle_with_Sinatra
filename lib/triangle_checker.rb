cd6dede40ec80e7b21bf03b9fd0e22fd724be7d3
Alma <a.q.frankenstein@gmail.com>
2020-09-28 14:18:28 -0700

    write method and pass test for is_isosceles
    Co-authored-by: Vanessa Stewart <vstewart1213@gmail.com>

--- lib/triangle_checker.rb
+++ lib/triangle_checker.rb
@@ -7,8 +7,22 @@ class TriangleChecker
     @side3 = side3
   end
 
-  def is_equilateral()
+  def is_equilateral?()
     @side1 == @side2 && @side2 == side3 ? true : false
+  end
+
+  def is_not_a_triangle?()
+    if ((@side1 + @side2) <= @side3) || ((@side1 + @side3) <= @side2) || ((@side3 + @side2) <= @side1)
+      return true
+    end
+    return false
+  end
+
+  def is_isosceles?()
+    if ((@side1 == @side2) || (@side1 == @side3) || (@side3 == @side2))
+      return true
+    end
+    return false
   end
 
 
--- spec/triangle_checker_spec.rb
+++ spec/triangle_checker_spec.rb
@@ -12,10 +12,24 @@ describe TriangleChecker do
     end
   end
 
-  describe('#is_equilateral') do
+  describe('#is_equilateral?') do
     it('checks whether a triangle is equilateral') do
       triCheck = TriangleChecker.new(3,4,5)
-      expect(triCheck.is_equilateral()).to(eq(false))
+      expect(triCheck.is_equilateral?()).to(eq(false))
+    end
+  end
+
+  describe('#is_not_a_triangle?') do
+    it('checks if a triangle is not a triangle') do
+      triCheck = TriangleChecker.new(2,2,100)
+      expect(triCheck.is_not_a_triangle?()).to(eq(true))
+    end
+  end
+
+  describe('#is_isosceles?') do
+    it('checks if a triangle is isosceles') do
+      triCheck = TriangleChecker.new(3,3,4)
+      expect(triCheck.is_isosceles?()).to(eq(true))
     end
   end
 end
